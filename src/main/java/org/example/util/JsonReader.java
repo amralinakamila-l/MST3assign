676423c11153419eef30cb4c84cd3d0c06a8958d
Камила <amralinakamila8114@gmail.com>
2025-10-23 02:09:50 +0500

    Initial project structure: packages and input.json

---
+++ src/main/java/org/example/util/JsonReader.java
@@ -0,0 +1,44 @@
+package org.example.util;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import org.example.model.*;
+
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.*;
+
+public class JsonReader {
+    public static List<Graph> loadGraphs(String filePath) {
+        try (FileReader reader = new FileReader(filePath)) {
+            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+            JsonArray graphsArray = jsonObject.getAsJsonArray("graphs");
+
+            List<Graph> graphs = new ArrayList<>();
+            for (JsonElement graphEl : graphsArray) {
+                JsonObject g = graphEl.getAsJsonObject();
+                int id = g.get("id").getAsInt();
+
+                List<String> nodes = new Gson().fromJson(g.get("nodes"),
+                        new TypeToken<List<String>>(){}.getType());
+
+                List<Edge> edges = new ArrayList<>();
+                for (JsonElement e : g.getAsJsonArray("edges")) {
+                    JsonObject eo = e.getAsJsonObject();
+                    edges.add(new Edge(
+                            eo.get("from").getAsString(),
+                            eo.get("to").getAsString(),
+                            eo.get("weight").getAsInt()
+                    ));
+                }
+
+                graphs.add(new Graph(id, nodes, edges));
+            }
+            return graphs;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+}
+
